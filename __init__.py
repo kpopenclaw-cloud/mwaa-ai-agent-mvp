11ae2aac1f8fc3c0d52272c2025d906282e57bc8
kpopenclaw-cloud <kp.openclaw@gmail.com>
2026-07-24 21:47:17 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .agent import MwaaDeps, FailureDiagnosis, build_agent, ask
-from .mwaa_client import MwaaClient
-
-__all__ = ["MwaaDeps", "FailureDiagnosis", "build_agent", "ask", "MwaaClient"]
